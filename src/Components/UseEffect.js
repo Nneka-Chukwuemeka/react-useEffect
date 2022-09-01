bdfc5d5f6379a408caba40eaac6e7cc4d6d77107
Nneka Chukwuemeka <nneka.uchechukwu.chukwuemeka@gmail.com>
2022-09-01 23:59:08 +0100

    used react hook useEffect to change background

---
+++ src/Components/UseEffect.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useEffect, useState } from 'react';
+import '../assets/UseEffect.css';
+
+function UseEffect() {
+  //  state ....
+  const [color, setColor] = useState('white');
+
+  useEffect(() => {
+    //Do Something
+    document.body.style.backgroundColor = getRandomColor();
+  }, [color]);
+
+  //   click Even Handler
+  const clickEventHandler = (event) => {
+    event.preventDefault();
+    setColor(getRandomColor());
+  };
+
+  const getRandomColor = () => {
+    return '#' + Math.random().toString(16).slice(2, 8);
+  };
+  return (
+    <div className="container">
+      <p> backgroundColor : {color} </p>
+      <button onClick={clickEventHandler}> change color</button>
+    </div>
+  );
+}
+
+export default UseEffect;
